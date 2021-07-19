e2382c23ea31752c6cdb19389ec06f1787891698
Himanshu Kumar <50806321+hk3199@users.noreply.github.com>
2021-07-19 20:26:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,112 @@
-const Socket =require("webSocket").server//websocket
-const http= require("http")
+const Socket = require("websocket").server
+const http = require("http")
 
-const server= http.createServer((req,res) => {})
+const server = http.createServer((req, res) => {})
 
- server.listen(3000, ()  => {
-     console.log("listening on port 3000...")
- })
+server.listen(3000, () => {
+    console.log("Listening on port 3000...")
+})
 
- const webSocket =new Socket({httpServer:server})
+const webSocket = new Socket({ httpServer: server })
 
- let names =[] //users
+let users = []
 
- webSocket.on('request', (req) => {
-     const data =JSON.parse(message.utf8Data)
-    
-      switch(data.type)  {
-          case "store_user":
-              if(user!=null)
-               const newUser={
-                   conn: connection,
-                   username: data.username
-               }
+webSocket.on('request', (req) => {
+    const connection = req.accept()
 
-               names.push(newUser)
-               console.log(newUser.username)
-               break
-      }
-       
+    connection.on('message', (message) => {
+        const data = JSON.parse(message.utf8Data)
 
- } )
+        const user = findUser(data.username)
 
- function finduser (username) {
-     for(let i=0;i<names.length;i++){
-         if(names[i].username= username)
-           return users[i]
-     }
- } 
+        switch(data.type) {
+            case "store_user":
+
+                if (user != null) {
+                    return
+                }
+
+                const newUser = {
+                     conn: connection,
+                     username: data.username
+                }
+
+                users.push(newUser)
+                console.log(newUser.username)
+                break
+            case "store_offer":
+                if (user == null)
+                    return
+                user.offer = data.offer
+                break
+            
+            case "store_candidate":
+                if (user == null) {
+                    return
+                }
+                if (user.candidates == null)
+                    user.candidates = []
+                
+                user.candidates.push(data.candidate)
+                break
+            case "send_answer":
+                if (user == null) {
+                    return
+                }
+
+                sendData({
+                    type: "answer",
+                    answer: data.answer
+                }, user.conn)
+                break
+            case "send_candidate":
+                if (user == null) {
+                    return
+                }
+
+                sendData({
+                    type: "candidate",
+                    candidate: data.candidate
+                }, user.conn)
+                break
+            case "join_call":
+                if (user == null) {
+                    return
+                }
+
+                sendData({
+                    type: "offer",
+                    offer: user.offer
+                }, connection)
+                
+                user.candidates.forEach(candidate => {
+                    sendData({
+                        type: "candidate",
+                        candidate: candidate
+                    }, connection)
+                })
+
+                break
+        }
+    })
+
+    connection.on('close', (reason, description) => {
+        users.forEach(user => {
+            if (user.conn == connection) {
+                users.splice(users.indexOf(user), 1)
+                return
+            }
+        })
+    })
+})
+
+function sendData(data, conn) {
+    conn.send(JSON.stringify(data))
+}
+
+function findUser(username) {
+    for (let i = 0;i < users.length;i++) {
+        if (users[i].username == username)
+            return users[i]
+    }
+}
