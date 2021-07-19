a0778b3d5e6345f30ff30e4fd683ce3868b6c3de
Himanshu Kumar <50806321+hk3199@users.noreply.github.com>
2021-07-19 20:40:53 +0530

    Update receiver.js

--- receiver/receiver.js
+++ receiver/receiver.js
@@ -1,4 +1,4 @@
-const webSocket= new WebSocket("ws://localhost:3000/")
+const webSocket= new WebSocket("ws://localhost:3000")
 
 webSocket.onmessage = (event) => {
     handleSignallingData(JSON.parse(event.data))
