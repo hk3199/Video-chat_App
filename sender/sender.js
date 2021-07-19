bf0445574d635c194884be7869bcc36bb9c5781b
Himanshu Kumar <50806321+hk3199@users.noreply.github.com>
2021-07-19 20:38:36 +0530

    Update sender.js

--- sender/sender.js
+++ sender/sender.js
@@ -50,7 +50,9 @@ function starter(){
         let configure={
             iceServers:[
                 {
-                   
+                    "urls": ["stun:stun.l.google.com:19302", 
+                    "stun:stun1.l.google.com:19302", 
+                    "stun:stun2.l.google.com:19302"]
                 }
             ]
         }
